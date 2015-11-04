98d0f045be31490d81f1b80339ae977a38db44fd
Bridger Holt <bridgerholt@Alveyworld-23.local>
2015-11-04 11:38:22 -0700

    Splitting almost complete

---
+++ code/chapter/enemy.js
@@ -0,0 +1,18 @@
+function Enemy(pos) {
+  this.pos = pos.plus(new Vector(0, -0.5));
+  this.size = new Vector(0.8, 1.5);
+  this.speed = new Vector(0, 0);
+}
+Enemy.prototype.type = "enemy";
+
+Enemy.prototype.act = function(step, level, keys) {
+  var otherActor = level.actorAt(this);
+  if (otherActor)
+    level.playerTouched(otherActor.type, otherActor);
+
+  // Losing animation
+  if (level.status == "lost") {
+    this.pos.y += step;
+    this.size.y -= step;
+  }
+};
